8c157af514ca9a5f92df7f8523d8fb27e0be0fe3
Jake Marshall <jakemarshall27@icloud.com>
2020-03-02 17:13:40 +0000

    Add user foreign key and description column to bookclubs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_143847) do
+ActiveRecord::Schema.define(version: 2020_03_02_171239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2020_03_02_143847) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.string "description"
+    t.index ["user_id"], name: "index_bookclubs_on_user_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -67,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_143847) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookclubs", "users"
   add_foreign_key "owners", "books"
   add_foreign_key "owners", "users"
   add_foreign_key "swaps", "users"
