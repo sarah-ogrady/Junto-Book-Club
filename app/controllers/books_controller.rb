e92c117b5a598a63ec61fda38f4468095e21488e
Jake Marshall <jakemarshall27@icloud.com>
2020-03-04 12:23:47 +0000

    Implemented API parse to get book info using ISBN, and book cover image from URL

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
@@ -5,5 +8,34 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    url_info = "https://www.googleapis.com/books/v1/volumes?q=isbn:#{@book.isbn}"
+
+    info_serialized = open(url_info).read
+    book_info = JSON.parse(info_serialized)
+
+    @book.title = book_info["items"]["volumeInfo"]["title"]
+    @book.author_name = book_info["items"]["volumeInfo"]["authors"]
+    @book.year = book_info["items"]["volumeInfo"]["publishedDate"].slice[0, 3]
+    @book.description = book_info["items"]["volumeInfo"]["description"]
+
+    cover_file = Uri.open("http://covers.openlibrary.org/b/isbn/#{@book.isbn}-L.jpg")
+    @book.photo.attach(io: cover_file, filename: 'cover.jpg', content_type: 'image/jpg')
+
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:isbn)
   end
 end
