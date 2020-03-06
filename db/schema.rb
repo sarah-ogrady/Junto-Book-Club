08582bc6bd89ddd3ec66f60f7bc79822a49459f9
Gareth Cary <gareth@caryfamily.co.uk>
2020-03-06 13:49:59 +0000

    edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2020_03_05_202716) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,17 +77,6 @@ ActiveRecord::Schema.define(version: 2020_03_05_202716) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "owners", force: :cascade do |t|
-    t.string "current"
-    t.boolean "reading"
-    t.bigint "user_id", null: false
-    t.bigint "book_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["book_id"], name: "index_owners_on_book_id"
-    t.index ["user_id"], name: "index_owners_on_user_id"
   end
 
   create_table "swaps", force: :cascade do |t|
