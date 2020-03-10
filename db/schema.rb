73893ac2b9c3bcb0a1b8a004cbb31fbf8156312a
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-03-10 23:24:10 +0000

    add images to seed, change buttons, edit bookclub index cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_154108) do
+ActiveRecord::Schema.define(version: 2020_03_10_183924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_154108) do
     t.string "description"
     t.float "longitude"
     t.float "latitude"
+    t.string "image_url"
     t.index ["user_id"], name: "index_bookclubs_on_user_id"
   end
 
